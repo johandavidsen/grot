574619ca172345301a9fc1b459f1f3361bafdcb7
johandavidsen <johan.davidsen@fjakkarin.com>
2016-02-03 13:33:07 +0100

    Added require on babeljs.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+require("babel-core/register")({
+    presets: [
+        "stage-0",
+        "react",
+        "es2015"
+    ]
+});
+
 import GrotHello from "./GrotHello";
 import GrotTable from './GrotTable';
 import GrotPanel from './GrotPanel';
