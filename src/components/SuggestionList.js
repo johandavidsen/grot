da67298e02900732ac6e020d5a144565272ed7d8
Jóhan Davidsen <johan.davidsen@fjakkarin.com>
2016-08-19 11:51:17 +0200

    New component Select

---
+++ src/components/SuggestionList.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import classNames from 'classNames'
+
+import '../stylesheets/components/_suggestion-list.scss'
+
+/**
+ *
+ */
+class SuggestionList extends React.Component {
+
+  /**
+   * @constructor
+   *
+   *
+   */
+  constructor (props) {
+    super(props)
+    this.state = {
+      activeItem: -1
+    }
+  }
+
+  /**
+   *
+   */
+  render ( ) {
+    const { highlightedItem, searchTerm, suggestions } = this.props
+    const { activeItem } = this.state
+
+    return (
+      <ul className="suggestions-list">
+      {suggestions.map((suggestion, index) =>
+        <li
+          className={classNames({
+            highlighted: highlightedItem === index || activeItem === index
+          })}
+          key={index}
+          onClick={() => this.props.onSelection(suggestion)}
+          onMouseEnter={() => this.setState({ activeItem: index })}
+          onMouseDown={(e) => e.preventDefault()}
+          >
+          <div className='suggestion-list-id'>{suggestion.id}</div>
+          <div className='suggestion-list-description'>{suggestion.description}</div>
+          <div className='suggestion-list-probability'>{ suggestion.probability}%</div>
+        </li>
+      )}
+      </ul>
+    )
+  }
+}
+
+/**
+ *
+ */
+export default SuggestionList
