68a454a33c68f65b52d06f43049fe99b3ec93bc1
Jóhan Davidsen <johan.davidsen@fjakkarin.com>
2016-08-20 16:36:32 +0200

    Updated the Credits component

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -11,10 +11,56 @@ import '../stylesheets/components/_credits.scss'
  * @author Jóhan Davidsen <johan.davidsen@fjakkarin.com>
  *
  */
-export default ( ) => (
-  <div className="row">
-    <div className="twelve columns">
-      This module was made by <a href="">Jóhan Davidsen</a>, source code can be found on <a href="">Bitbucket</a> and the module is available on <a href="">NPM</a>.
-    </div>
-  </div>
-)
+class Credits extends React.Component {
+
+  /**
+   * @method render
+   *
+   * This function returns the JSX object.
+   *
+   */
+  render () {
+   return (
+     <div>
+       <p>
+       This module was made by <a href={this.props.authorUrl}>{this.props.author}</a>,<br />
+       source code can be found on <a href={this.props.gitUrl}>{this.props.git}</a><br />
+       and the module is available on <a href={this.props.distUrl}>{this.props.dist}</a>.
+       </p>
+     </div>
+   )
+  }
+}
+
+/**
+ * @prop PropTypes
+ *
+ * These are the properties, which are required by the component.
+ */
+Credits.PropTypes = {
+  author: React.PropTypes.string.isRequired,
+  authorUrl: React.PropTypes.string.isRequired,
+  git: React.PropTypes.string.isRequired,
+  gitUrl: React.PropTypes.string.isRequired,
+  dist: React.PropTypes.string.isRequired,
+  distUrl: React.PropTypes.string.isRequired
+}
+
+/**
+ * @prop defaultProps
+ *
+ * These are the default values of the properties for this component.
+ */
+Credits.defaultProps = {
+  author: 'Jóhan Davidsen',
+  authorUrl: 'http://fjakkarin.com',
+  git: 'GitHub',
+  gitUrl: '',
+  dist: 'NPM',
+  distUrl: 'npmjs'
+}
+
+/**
+ * The exported object
+ */
+export default Credits
