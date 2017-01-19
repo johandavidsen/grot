f82eb9e8033d7b8342b9fd23c299a6f46b0e47b3
Jóhan Davidsen <johan.davidsen@fjakkarin.com>
2017-01-19 17:34:33 +0100

    Add style and cleaned up the component

--- src/components/hello/index.js
+++ src/components/hello/index.js
@@ -1,2 +1,22 @@
-import Hello from './Hello'
-export default Hello
+import React from 'react'
+
+import './Hello.scss'
+
+/**
+ * @class Hello
+ *
+ * This is the HelloWorld component for the Grót Module. This function only
+ * display a simple HTML string.
+ *
+ * @since 0.1.1
+ * @author Jóhan Davidsen <johan.davidsen@fjakkarin.com>
+ *
+ */
+export default () => (
+  <div className='grot-hello-world'>
+    <h1>
+      Hello from Grót
+    </h1>
+    <h5>A sample library developed by <a href='http://fjakkarin.com'>Jóhan Davidsen</a></h5>
+  </div>
+)
